2260fca20e71ae31fc33bd974dd97612a1fe4e3b
ivanlis <ivanlis.bilbao@gmail.com>
2018-04-15 21:41:13 +0200

    Implement the matrix with a cached inverse. For Week 3 assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions to handle the 'special' matrix,
+## containing a cache for its inverse.
 
-## Write a short comment describing this function
+## Creates a list of functions to handle the matrix x and
+## the cached inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    # cache for the inverse matrix
+    cacheInv <- NULL
+    
+    set <- function(newMatrix) {
+        x <<- newMatrix
+        message("Invalidating the cache...")
+        cacheInv <<- NULL
+    }
+    
+    get <- function() {
+        x
+    }
+    
+    setInv <- function(invMatrix) {
+        cacheInv <<- invMatrix
+    }
+    
+    getInv <- function() {
+        cacheInv
+    }
+    
+    # return the list of functions
+    list(set = set, get = get, setInv = setInv, getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+## Retrieves the inverse matrix from the cache, 
+## if the cache is not available, computes the inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    
+    invMatrix <- x$getInv()
+    
+    if (is.null(invMatrix))
+    {
+        # actually compute the inverse matrix
+        invMatrix <- solve(x$get(), ...)
+        # set the cache in the special matrix
+        x$setInv(invMatrix)
+    }
+    else
+    {
+        message("Getting cached inverse matrix...")
+    }
+    
+    invMatrix
 }
